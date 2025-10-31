f1530c3b391163edbe51a704b70a7eaeb3e9b404
Xico <xico@ufersa.edu.br>
2025-10-30 21:44:32 -0300

    Create MochilaFracionaria.java

---
+++ seiUsarUmComputador/unidade2/exercicio4/MochilaFracionaria.java
@@ -0,0 +1,22 @@
+public class MochilaFracionaria {
+    public static void main(String[] args) {
+        // Capacidade da mochila
+        int capacidade = ?; // 50?
+        
+        // Itens: peso, valor
+        int[] pesos = {?}; // 10, 20, 30?
+        int[] valores = {?}; // 60, 100, 120?
+        
+        // Calcular valor por peso
+        double[] valorPorPeso = new double[pesos.length];
+        for (int i = 0; i < pesos.length; i++) {
+            valorPorPeso[i] = (double) valores[i] / pesos[i];
+        }
+        
+        // Implementar algoritmo guloso
+        // Ordenar por valor/peso decrescente
+        // Adicionar itens até capacidade máxima
+        
+        System.out.println("Valor máximo obtido: " + valorMaximo);
+    }
+}
